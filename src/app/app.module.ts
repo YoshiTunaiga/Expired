c92e1121d53247b5a6e3b4190f363e21fe2e45b5
Gi Diaz <yandelydf@gmail.com>
2021-12-14 19:44:09 -0500

    (Feat): Starting a Navbar Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { LoadingScreenComponent } from './loading-screen/loading-screen.component';
+import { StoragesComponent } from './storages/storages.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    LoadingScreenComponent
+    LoadingScreenComponent,
+    StoragesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -19,6 +23,7 @@ import { LoadingScreenComponent } from './loading-screen/loading-screen.componen
     RouterModule.forRoot([
       {path: '', component: LoadingScreenComponent},
       {path: 'homepage', component: HomepageComponent},
+      {path: 'storages', component: StoragesComponent},
     ])
   ],
   providers: [],
---
+++ src/app/storages.ts
@@ -0,0 +1,19 @@
+export interface Storage{
+  name: string,
+  imageUrl: string
+}
+
+export const storage = [
+  {
+    name: "Fridge",
+    imageUrl: "https://media.istockphoto.com/vectors/modern-vertical-refrigerator-with-open-transparent-front-panel-vector-id607621000?k=20&m=607621000&s=612x612&w=0&h=oBfUJ0-WC0r4GDEFHm6t_0QZSxgcMqYTlS6zR-KqX1M="
+  },
+  {
+    name: "Cabinet",
+    imageUrl: "https://us.123rf.com/450wm/anatolir/anatolir1806/anatolir180602115/103220736-open-glass-fridge-icon-outline-illustration-of-open-glass-fridge-vector-icon-for-web-design-isolated.jpg?ver=6"
+  },
+  {
+    name: "Drawer",
+    imageUrl: "https://cdn0.iconfinder.com/data/icons/stationery-1/512/archive-512.png"
+  },
+]
